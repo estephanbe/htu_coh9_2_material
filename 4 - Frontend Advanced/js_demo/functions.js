25c816d04f8ae6175746b3132ecfd550cebd88d0
Bisharah Estephan <macbosh@Bisharahs-MacBook-Pro.local>
2022-10-27 18:12:50 +0300

    update

--- 4 - Frontend Advanced/js_demo/functions.js
+++ 4 - Frontend Advanced/js_demo/functions.js
@@ -23,3 +23,31 @@ let arr = [1, 2, 3];
 
 arr.push(4);
 // console.log(arr);
+
+let Obj = {
+  name: "Khalid",
+  age: 20,
+  speek: function () {
+    console.log("This is Khalid");
+  },
+};
+
+function printHi() {
+  console.log("hi");
+}
+
+function getGreeting() {
+  return "hi";
+}
+
+let a = 1;
+let b = 3;
+// console.log(a + b);
+
+function addTo(a, b) {
+  return a + b;
+}
+// console.log(addTo(1, 3));
+// console.log(addTo(4, 5));
+
+// Array methods
---
+++ 4 - Frontend Advanced/js_demo/party.js
@@ -0,0 +1,9 @@
+// Security Gaurd, allows people to enter to a party, only people between 18 and 35 years old can enter the party
+
+// Loop through number of people.
+do {
+  // ask each user about his/her age.
+  // if younger than 18, print: you are too young
+  // if older than 35, print: you are too old
+  // if between 18 and 35, ask the user about his/her name and welcome the user to the party.
+} while (confirm("is there anyone else?"));
